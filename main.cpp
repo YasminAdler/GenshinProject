f52c5a2dd7d465c91f7a7e3d1115a643392c4f6f
YasminAdler <adleryasmin@gmail.com>
2023-01-06 17:26:50 +0200

    changing to &&

--- main.cpp
+++ main.cpp
@@ -9,18 +9,17 @@ using namespace std;
 
 // addNewCharacterToPlayer
 
-bool addNewCharacterToPlayer(Player &player) // crushing
+void addNewCharacterToPlayer(Player &player) // crushing
 {
      char name[CHAR_MAX];
      char input = '0';
      cout << "Character name:\t";
-     std::cin >> name;
+     cin >> name;
      cout << "Character type(E - Epic or L - legend):" << endl;
-
-     while (input != 'E' || input != 'L')
+     do
      {
-          std::cin >> input;
-          std::cin.clear();
+          cout << "ITeration\n";
+          cin >> input;
           switch (input)
           {
           case 'E':
@@ -39,46 +38,9 @@ bool addNewCharacterToPlayer(Player &player) // crushing
           }
           default:
                cout << "invald key" << endl;
-               break;
           }
-     }
-     return true;
+     }while ((input != 'E') && (input != 'L'));
 }
-
-// bool addNewCharacterToPlayer(Player &player) // crushing
-// {
-//      char name[CHAR_MAX];
-//      char input = '0';
-//      cout << "Character name:\t";
-//      cin >> name;
-//      cout << "Character type(E - Epic or L - legend):" << endl;
-//      do
-//      {
-//           cin >> input;
-//           cin.clear();
-//           switch (input)
-//           {
-//           case 'E':
-//           {
-//                EpicCharacter *character = new EpicCharacter();
-//                character->setName(name);
-//                player.addCharacterToData(character);
-//                break;
-//           }
-//           case 'L':
-//           {
-//                LegendaryCharacter *character = new LegendaryCharacter();
-//                character->setName(name);
-//                player.addCharacterToData(character);
-//                break;
-//           }
-//           default:
-//                cout << "invald key" << endl;
-//                break;
-//           }
-//      } while (input != 'E' || input != 'L');
-//      return true;
-// }
 
 void editWeaponToCharacter(Player &player)
 {
@@ -91,8 +53,8 @@ void editWeaponToCharacter(Player &player)
           << "d. " << player.getActiveCharacter(3) << endl;
      do
      {
-          cin >> input;
-          cin.clear();
+          std::cin >> input;
+          std::cin.clear();
           if (input < 'a' || input > 'd')
                cout << "Invalid input";
      } while (input < 'a' || input > 'd');
@@ -112,8 +74,8 @@ bool editActiveTeam(Player &player)
           {
                cout << i + 1 << " " << player.getActiveCharacter(i) << endl;
           }
-          cin >> input;
-          cin.clear();
+          std::cin >> input;
+          std::cin.clear();
           characterNumber = stoi(input);
           if (characterNumber < 0 || characterNumber >= (int)numOfCharachters)
                cout << "Invalid number" << endl;
@@ -123,8 +85,8 @@ bool editActiveTeam(Player &player)
           << "b. " << player.getActiveCharacter(1) << "\n"
           << "c. " << player.getActiveCharacter(2) << "\n"
           << "d. " << player.getActiveCharacter(3) << endl;
-     cin >> input;
-     cin.clear();
+     std::cin >> input;
+     std::cin.clear();
      player.setActiveCharacter((unsigned)(input[0] - 'a'), player.getDataCharacter((unsigned)characterNumber));
      return true;
 }
@@ -133,9 +95,9 @@ int main()
 {
      Player player{};
      char name[CHAR_MAX];
-     char input = '0';
+     char mainInput = '0';
      cout << "Player name:\t";
-     cin >> name;
+     std::cin >> name;
      player.setPlayerName(name);
      // cin.clear();
      cout << "1 Add New Character to data \n"
@@ -145,9 +107,9 @@ int main()
           << "0 Exit" << endl;
      do
      {
-          cin >> input;
-          cin.clear(); // need to check ***
-          switch (input)
+          std::cin >> mainInput;
+          std::cin.clear(); // need to check ***
+          switch (mainInput)
           {
           case '0':
                cout << "See Ya!" << endl;
@@ -168,6 +130,6 @@ int main()
                cout << "invalid key" << endl;
                break;
           }
-     } while (input != '0');
+     } while (mainInput != '0');
      return 0;
 }
